8eace2119b1cdf3d6789bfaff497adc1129d897f
Hardik <hardik8464@iitkgp.ac.in>
2021-01-20 16:54:24 +0530

    1e updated

--- Assignment_1/Assgn1_1e_37_18CS10021_18CS30040.sh
+++ Assignment_1/Assgn1_1e_37_18CS10021_18CS30040.sh
@@ -1,1 +1,6 @@
-head /dev/urandom | tr -dc A-Za-z0-9_ | head -c${1:-16}
+upp=$(tr -dc 'A-Z' </dev/urandom|head -c1)
+low=$(tr -dc 'a-z' </dev/urandom|head -c1)
+dig=$(tr -dc '0-9' </dev/urandom|head -c1)
+spe=$(tr -dc '_' </dev/urandom|head -c1)
+printf $upp$low$dig$spe$(cat /dev/urandom | tr -dc A-Za-z0-9_ | head -c $((${1:-16}-4)))|sed 's/./&\n/g'|shuf|tr -d "\n"
+
