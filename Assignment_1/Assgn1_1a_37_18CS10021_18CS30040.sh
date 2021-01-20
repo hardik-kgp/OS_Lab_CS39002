fabbbf1899457275fa6406718c265b8a1694fe4e
Hardik <hardik8464@iitkgp.ac.in>
2021-01-20 20:43:59 +0530

    fixed 1a

--- Assignment_1/Assgn1_1a_37_18CS10021_18CS30040.sh
+++ Assignment_1/Assgn1_1a_37_18CS10021_18CS30040.sh
@@ -1,6 +1,9 @@
-if [[ $1 =~ "[0-9][0-9,]*" ]]; then echo "Error"; fi #doubt
+if [[ $1 =~ "[0-9][0-9,]*" ]]; then echo "Error";exit 1; fi 
 gcd(){
     until test 0 -eq "$2";do set -- "$2" "$(($1 % $2))";done;if [ 0 -gt "$1" ];then echo "$((- $1))";else  echo "$1"; fi
 }
 IFS=',' read -a array <<< $1
-for i in ${array[@]};do array[0]=$(gcd ${array[0]} $i) ;done;echo ${array[0]}
+cnt=0
+for i in ${array[@]};do array[0]=$(gcd ${array[0]} $i);((cnt=cnt+1));done;
+if [[ $cnt -gt 10 ]]; then echo "Error"; exit 1 ;fi
+echo ${array[0]}
