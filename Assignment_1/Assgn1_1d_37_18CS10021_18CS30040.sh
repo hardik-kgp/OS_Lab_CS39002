4c2cf0652fb015fb0ff95db543016740b3ec9c1c
Sriyash <poddarsriyash@gmail.com>
2021-01-21 11:48:01 +0530

    1d done

--- Assignment_1/Assgn1_1d_37_18CS10021_18CS30040.sh
+++ Assignment_1/Assgn1_1d_37_18CS10021_18CS30040.sh
@@ -1,28 +1,15 @@
 case ${1##*.} in
-    "tar.bz2")
-    ;&
-    "tbz2")
-    ;&
-    "tbz") tar -xvjf $1
-    ;;
-    "tgz")
-    ;&
-    "tar.gz") tar -xvzf $1
-    ;;
-    "Z")
-    ;&
-    "gz") gzip -d $1
-    ;;
-    "zip")
-    ;&
-    "7z")
-    ;&
-    "rar") 7z x $1
-    ;;
-    "tar") tar -xvf $1
-    ;;
-    "bz2") bzip2 -d $1
-    ;;
+    "tar.bz2");&
+    "tbz2");&
+    "tbz") tar -xvjf $1;;
+    "tgz");&
+    "tar.gz") tar -xvzf $1;;
+    "Z");&
+    "gz") gzip -d $1;;
+    "zip");&
+    "7z");&
+    "rar") 7z x $1;;
+    "tar") tar -xvf $1;;
+    "bz2") bzip2 -d $1;;
+    *) echo "Unknown file format: cannot extract"
 esac
-
-#to be tested properly
