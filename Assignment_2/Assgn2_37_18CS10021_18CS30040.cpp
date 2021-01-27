2fb35c59e77d4e6da5b9615110b52a0dce1ffd81
Sriyash <poddarsriyash@gmail.com>
2021-01-28 02:34:33 +0530

    comments added

--- Assignment_2/Assgn2_37_18CS10021_18CS30040.cpp
+++ Assignment_2/Assgn2_37_18CS10021_18CS30040.cpp
@@ -12,28 +12,8 @@ using namespace std;
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 
-class Command
-{
-    public:
-        string cmd;
-        vector <string> args;
-        string fin;
-        string fout;
-        int bkg;
-        Command(string);
-        void parse();
-        void io_redirect();
-        void execute();
-};
-
-Command::Command(string cmd){
-    this->cmd=cmd;
-    fin="";
-    fout="";
-    bkg=0;
-}
-
 vector<string> mystrtok(string s, char c){
+    /*Function to tokenize string s using delimiter c*/
     vector<string> tokens;
     stringstream ss(s);
     string temp;
@@ -42,37 +22,55 @@ vector<string> mystrtok(string s, char c){
         tokens.push_back(temp);
     }
     return tokens;
-    
 }
 
+class Command{
+    /*Class to store data about the command*/
+public:
+    string cmd;             // stores the command string
+    vector<string> args;    // stores the args to execvp
+    string fin;             // stores name of input file, if any
+    string fout;            // stores name of output file, if any
+    int bkg;                // flag to denote, if to run a process in background or not
+    Command(string);
+    void parse();
+    void io_redirect();
+    void execute();
+};
 
-
+Command::Command(string cmd){
+    /*Class Constructor*/
+    this->cmd = cmd;
+    fin = "";
+    fout = "";
+    bkg = 0;
+}
 
 void Command::parse(){
-    
+    /*Function to parse command string*/
     vector<string> tokens = mystrtok(cmd, SHELL_TOK_DELIM);
-    
-    for(int i = 0;i<tokens.size();i++){
+
+    for (int i = 0; i < tokens.size(); i++){
         string token = tokens[i];
-        if(token != ""){
-            if(token == "&"){
+        if (token != ""){// Ignoring empty tokens
+            if (token == "&"){// & denotes process to run in background
                 bkg = 1;
-            }   
-            else if(token == "<"){
-                if(i + 1 == tokens.size()){
-                    cout << "Input Not specified!\n";
-                    exit(0);
+            }
+            else if (token == "<"){// input redirection
+                if (i + 1 == tokens.size()){
+                    cout << "Input file not specified!\n";
+                    exit(EXIT_FAILURE);
                 }
                 fin = tokens[++i];
             }
-            else if(token == ">"){
-                if(i + 1 == tokens.size()){
+            else if (token == ">"){// output redirection
+                if (i + 1 == tokens.size()){
                     cout << "Output Not specified!\n";
-                    exit(0);
+                    exit(EXIT_FAILURE);
                 }
                 fout = tokens[++i];
             }
-            else{
+            else{// args
                 args.push_back(token);
             }
         }
@@ -80,40 +78,41 @@ void Command::parse(){
     return;
 }
 
-void Command::io_redirect(){  
-    if(fin != ""){
-        int fd = open(fin.c_str(), O_RDONLY);
-        if(fd < 0){
-            cout << "FILE CANNOT BE OPENED. ERROR !" << endl;
-            exit(0);
+void Command::io_redirect(){
+    /*Function to redirect input and output*/
+    if (fin != ""){
+        int fd = open(fin.c_str(), O_RDONLY); // open file in read only
+        if (fd < 0){
+            cout << "Cannot open file!" << endl;
+            exit(EXIT_FAILURE);
         }
-        
-        int status = dup2(fd, STDIN_FILENO);
-        if(status < 0){
+        int status = dup2(fd, STDIN_FILENO); // redirect from stdin to fin
+        if (status < 0){
             cout << "Input redirection failed !" << endl;
-            exit(0);
+            exit(EXIT_FAILURE);
         }
     }
-    if(fout != ""){
-        int fd = open(fout.c_str(),  O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-        if(fd < 0){
-            cout << "FILE CANNOT BE OPENED. ERROR !" << endl;
+
+    if (fout != ""){
+        int fd = open(fout.c_str(), O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU); // open file in write only
+        if (fd < 0){
+            cout << "Cannot open file!" << endl;
             exit(0);
         }
-        
-        int status = dup2(fd, STDOUT_FILENO);
-        if(status < 0){
-            cout << "OUTPUT redirection failed" << endl;
+        int status = dup2(fd, STDOUT_FILENO); // redirect from stdout to fout
+        if (status < 0){
+            cout << "Output redirection failed" << endl;
             exit(0);
         }
     }
 }
 
 void Command::execute(){
-    char* argv[args.size() + 1];
+    /*Function to execute command*/
+    char *argv[args.size() + 1];
     int i = 0;
-    for(string arg: args){
-        
+    for (string arg : args){
+        /*convert strings to char* */
         argv[i++] = (char *)(arg.c_str());
     }
     argv[i++] = NULL;
@@ -121,13 +120,11 @@ void Command::execute(){
     execvp(argv[0], argv);
 }
 
-vector <Command> split_line(string line){
-    
+vector<Command> split_line(string line){
+    /*Function to split piped command to simple commands*/
     vector<string> tokens = mystrtok(line, SHELL_PIPE_DELIM);
-    
-    vector <Command> cmds;
-    for(auto token : tokens){
-        
+    vector<Command> cmds;
+    for (auto token : tokens){
         Command cmd = Command(token);
         cmd.parse();
         cmds.push_back(cmd);
@@ -136,82 +133,77 @@ vector <Command> split_line(string line){
 }
 
 void shell_loop(){
+    /*main loop*/
     int status = 1;
     string line;
-    do
-    {
-        printf(">> ");
+    do{
+        printf(">> ");  //shell prompt
         getline(cin, line);
-        vector <Command> cmds = split_line(line);
-        if(cmds.size() == 1){
-            pid_t pid = fork();
-            if(pid == 0){
-                cmds[0].io_redirect();
+        vector<Command> cmds = split_line(line);
+        if (cmds.size() == 1){ // single command
+            pid_t pid = fork(); // child process
+            if (pid == 0){
+                cmds[0].io_redirect(); // execute command
                 cmds[0].execute();
-                exit(0);
+                exit(EXIT_FAILURE); // exit child process incase of failure
             }
             else{
-                if(cmds[0].bkg == 0){
-                    wait(&status);
+                if (cmds[0].bkg == 0){
+                    wait(&status); // wait if child not a background process
                 }
                 else{
                     cout << "[BG] " << pid << endl;
                 }
             }
-            
         }
-        else{
-            int no_of_cmds = cmds.size();
+        else {
+            int num_cmds = cmds.size();
             int newFD[2], FD[2];
-            for(int i = 0;i<no_of_cmds;i++){
-                if(i + 1< no_of_cmds) pipe(newFD);
-                pid_t pid = fork();
-                if(pid == 0){
-                    if(i == 0 || i + 1 == no_of_cmds){
+            for (int i = 0; i < num_cmds; i++){ // iterating through commands
+                if (i + 1 < num_cmds)
+                    pipe(newFD); // creating pipe
+                pid_t pid = fork(); //creating child process
+                if (pid == 0){
+                    if (i == 0 || i + 1 == num_cmds){ // redirect to i/o
                         cmds[i].io_redirect();
                     }
-                    if(i){
+                    if (i){// input piping
                         dup2(FD[0], 0);
                         close(FD[0]);
                         close(FD[1]);
                     }
-                    if(i + 1 < no_of_cmds){
+                    if (i + 1 < num_cmds){ // output piping
                         close(newFD[0]);
                         dup2(newFD[1], 1);
                         close(newFD[1]);
                     }
-
                     cmds[i].execute();
                 }
                 else{
-                    if(cmds[i].bkg != 0){
-                        wait(NULL);
+                    if (cmds[i].bkg != 0){
+                        wait(NULL); // wait if child not a background process
                     }
                 }
-                
-                if(i != 0){
+
+                if (i != 0){ // closing previous pipes
                     close(FD[0]);
                     close(FD[1]);
                 }
-                if(i + 1 != no_of_cmds){
+                if (i + 1 != num_cmds){ // piping fds
                     FD[0] = newFD[0], FD[1] = newFD[1];
                 }
             }
 
-            if(cmds.back().bkg == 0){
-                
-                while(wait(&status) > 0);
+            if (cmds.back().bkg == 0){ // wait if child not a background process
+                while (wait(&status) > 0)
+                    ;
             }
         }
-       
     } while (true);
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     shell_loop();
-    
     return EXIT_SUCCESS;
 }
-
-
-
