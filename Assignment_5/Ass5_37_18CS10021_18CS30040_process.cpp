dbb069f16488b02b3739d6dac64599219f5f3579
Hardik Aggarwal <hardik8464@gmail.com>
2021-03-11 01:00:13 +0530

    doc updated

--- Assignment_5/Ass5_37_18CS10021_18CS30040_process.cpp
+++ Assignment_5/Ass5_37_18CS10021_18CS30040_process.cpp
@@ -1,3 +1,10 @@
+/*Name: Hardik Aggarwal 
+Roll : 18CS10021
+
+Name: Sriyash Poddar
+Roll: 18CS30040
+
+*/
 #include <stdio.h>
 #include <unistd.h> 
 #include <stdlib.h>
@@ -5,7 +12,8 @@
 #include <time.h>
 #include <pthread.h>
 #include <sys/ipc.h> 
-#include <sys/shm.h> 
+#include <sys/shm.h>
+#include <sys/wait.h> 
 #include <sys/time.h>
 #include <queue>
 #include <vector>
@@ -36,32 +44,47 @@ typedef struct _job
     pid_t prod_pid;
     int prod_num;
     int priority;
-    time_t compute_time;
+    time_t compute_time = 0;
     pid_t pid;
 }JOB;
 
-struct ComparePriority{
-    bool operator()(JOB * const& a, JOB * const& b){
-        return a->priority < b->priority;
-    }  
-};
 
 typedef struct _shared_data
 {
-    priority_queue <JOB *, vector<JOB*>, ComparePriority> job_queue;
+    JOB job_queue[QUEUE_SIZE];
     int job_created;
     int job_completed;
     int max_size;
+    int queue_size;
     int max_jobs;
     pthread_mutex_t lock;
 }SHARED_DATA;
 
+JOB pop(SHARED_DATA *SHM){
+    int max_pos = 0;
+    for(int i=1; i<SHM->queue_size; i++){
+        if(SHM->job_queue[max_pos].priority < SHM->job_queue[i].priority)
+            max_pos = i;
+    }
+    JOB j = SHM->job_queue[max_pos];
+    SHM->queue_size -= 1;
+    for(int i=max_pos; i<SHM->queue_size; i++){
+        SHM->job_queue[i] = SHM->job_queue[i+1];
+    }
+    return j;
+}
+
+void push(SHARED_DATA *SHM, JOB j){
+    SHM->job_queue[SHM->queue_size] = j;
+    SHM->queue_size += 1;
+}
+
 int create_shared_data(){
     key_t key1 = ftok("/dev/random", 'b');
-	int shmid = shmget(key1,sizeof(SHARED_DATA),0660|IPC_CREAT);
-	if (shmid<0) {
-		error("Failed to allocate shared memory!\n");
-	}
+    int shmid = shmget(key1,sizeof(SHARED_DATA),0660|IPC_CREAT);
+    if (shmid<0) {
+        error("Failed to allocate shared memory!\n");
+    }
     return shmid;
 }
 
@@ -69,51 +92,51 @@ SHARED_DATA *get_shared_data(int shmid, int max_jobs){
     SHARED_DATA *SHM = (SHARED_DATA *)shmat(shmid, (void *)0, 0);
     SHM->job_created = 0;
     SHM->job_completed = 0;
+    SHM->queue_size = 0;
     SHM->max_size = QUEUE_SIZE;
     SHM->max_jobs = max_jobs;
 
     // initialising mutex locking
-	pthread_mutexattr_t lock_attr;
-	pthread_mutexattr_init(&lock_attr);
-	pthread_mutexattr_setpshared(&lock_attr, PTHREAD_PROCESS_SHARED);
-	pthread_mutex_init(&SHM->lock, &lock_attr);
-    pthread_mutex_unlock(&SHM->lock);
-	return SHM;
+    pthread_mutexattr_t lock_attr;
+    pthread_mutexattr_init(&lock_attr);
+    pthread_mutexattr_setpshared(&lock_attr, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&SHM->lock, &lock_attr);
+
+    return SHM;
 }
 
-JOB *create_job(pid_t prod_pid, int prod_num){
-    JOB *temp = (JOB *)malloc(sizeof(JOB));
-    temp->prod_pid = prod_pid;
-    temp->prod_num = prod_num;
-    temp->priority = rand()%PRI_MAX + 1;
-    temp->compute_time = rand()%COMPUTE_MAX + 1;
-    temp->pid = rand()%ID_MAX + 1;
+JOB create_job(pid_t prod_pid, int prod_num){
+    JOB temp;
+    temp.prod_pid = prod_pid;
+    temp.prod_num = prod_num;
+    temp.priority = rand()%PRI_MAX + 1;
+    temp.compute_time = rand()%COMPUTE_MAX + 1;
+    temp.pid = rand()%ID_MAX + 1;
     return temp;
 }
 
-void printjob(JOB *j)
+void printjob(JOB j)
 {
-	cout<<"Producer: "<<j->prod_num<<"\t";
-    cout<<"Producer pid: "<<j->prod_pid<<"\t";
-	cout<<"Priority: "<<j->priority<<"\t";
-	cout<<"Compute Time: "<<j->compute_time<<"\t";
-	cout<<"Job ID: "<<j->pid<<"\n\n";
+    cout<<"Producer: "<<j.prod_num<<"\t";
+    cout<<"Producer pid: "<<j.prod_pid<<"\t";
+    cout<<"Priority: "<<j.priority<<"\t";
+    cout<<"Compute Time: "<<j.compute_time<<"\t";
+    cout<<"Job ID: "<<j.pid<<"\n\n";
 }
 
 bool insert_job(SHARED_DATA *SHM, pid_t prod_pid, int prod_num){
-    cout << "Inserting job" << endl;
-    JOB *temp = create_job(prod_pid, prod_num);
+    JOB temp = create_job(prod_pid, prod_num);
     bool done = false;
     while (true){
         pthread_mutex_lock(&SHM->lock);
-        cout << "Add lock received" << endl;
+        
         if(SHM->job_created == SHM->max_jobs){
             done = true;
             pthread_mutex_unlock(&SHM->lock);
             break;
         }
-        else if(SHM->job_queue.size() < SHM->max_size){
-            SHM->job_queue.push(temp);
+        else if(SHM->queue_size < SHM->max_size){
+            push(SHM, temp);
             SHM->job_created += 1;
             if(SHM->job_created == SHM->max_jobs) done = true;
             printjob(temp);
@@ -126,31 +149,27 @@ bool insert_job(SHARED_DATA *SHM, pid_t prod_pid, int prod_num){
 }
 
 void producer_process(SHARED_DATA *SHM, int pid, int num){
-    cout << "Producer process" << endl;
     bool done = false;
     while(!done){
         delay(rand()%4);
         done = insert_job(SHM, pid, num);
     }
+    shmdt(SHM);
 }
 
 bool remove_job(SHARED_DATA *SHM, pid_t con_pid, int con_num){
-    cout << "Removing job" << endl;
-    JOB *temp;
+    JOB temp;
     bool done = false;
     while (true){
         pthread_mutex_lock(&SHM->lock);
-        cout << "REmove lock received" << endl;
         if(SHM->job_completed == SHM->max_jobs){
             done = true;
             pthread_mutex_unlock(&SHM->lock);
             break;
         }
-        else if(SHM->job_queue.size() > 0){
-            temp = SHM->job_queue.top();
-            SHM->job_queue.pop();
-            SHM->job_completed += 1;
-            if(SHM->job_completed == SHM->max_jobs) done = true;
+        else if(SHM->queue_size > 0){
+            temp = pop(SHM);
+           
             cout<<"Consumer: "<<con_num<<"\t";
             cout<<"Consumer pid: "<<con_pid<<"\t";
             printjob(temp);
@@ -159,17 +178,23 @@ bool remove_job(SHARED_DATA *SHM, pid_t con_pid, int con_num){
         }
         pthread_mutex_unlock(&SHM->lock);
     }
-    delay(temp->compute_time);
+    pthread_mutex_lock(&SHM->lock);
+    delay(temp.compute_time);
+    if(temp.compute_time != 0){
+        SHM->job_completed += 1;
+        if(SHM->job_completed == SHM->max_jobs) done = true;    
+    }
+    pthread_mutex_unlock(&SHM->lock);
     return done;
 }
 
 void consumer_process(SHARED_DATA *SHM, int pid, int num){
-    cout << "Consumer process created" << endl;
     bool done = false;
     while(!done){
         delay(rand()%4);
         done = remove_job(SHM, pid, num);
     }
+    shmdt(SHM);
 }
 
 int main(){
@@ -193,7 +218,7 @@ int main(){
         pid = fork();
         if(pid < 0) error("Error in creating producer process\n");
         if(pid == 0){
-            srand(time(0));
+            srand(time(0)^i*7);
             producer_process(SHM, getpid(), i);
             return 0;
         }
@@ -203,22 +228,24 @@ int main(){
         pid = fork();
         if(pid < 0) error("Error in creating consumer process\n");
         if(pid == 0){
-            srand(time(0));
+            srand(time(0)^i*11);
             consumer_process(SHM, getpid(), i);
             return 0;
         }
     }
 
+    
+
     while(true){
-		pthread_mutex_lock(&SHM->lock);
-        // cout << "Size of queue: " << SHM->job_queue.size() << "/" << SHM->max_size << endl;
-		if((SHM->job_completed) == max_jobs && (SHM->job_created) == max_jobs){
-			auto consumed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-begin);
-			cout<<"Jobs Executed: "<< max_jobs <<" Time Taken "<< (float)consumed.count()/1000000 <<"s."<<endl;
-			pthread_mutex_unlock(&SHM->lock);
-			break;
-		}
-		pthread_mutex_unlock(&SHM->lock);
-	}
-	return 0;
+        pthread_mutex_lock(&SHM->lock);
+        if((SHM->job_completed) == max_jobs && (SHM->job_created) == max_jobs){
+            auto consumed = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-begin);
+            cout<<"Jobs Executed: "<< max_jobs <<" Time Taken "<< (float)consumed.count()/1000000 <<"s."<<endl;
+            pthread_mutex_unlock(&SHM->lock);
+            break;
+        }
+        pthread_mutex_unlock(&SHM->lock);
+    }
+    shmdt(SHM);
+    return 0;
 }
